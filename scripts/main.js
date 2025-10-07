7bef245a30862f0a9981661209c56f07f40608ca
Filipp <wolverinexdd@yandex.ru>
2025-10-07 22:39:15 +0400

    added resources section

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,17 @@ import TabsCollection from "./Tabs.js";
 new Header()
 new TabsCollection()
 
+setTimeout(() => {
+console.log('setTimeout');
+}, 0);
+
+const promise = new Promise((resolve) => {
+console.log('Promise');
+resolve();
+});
+
+promise.then(() => {
+console.log('Promise resolve');
+});
+
+console.log('End');
